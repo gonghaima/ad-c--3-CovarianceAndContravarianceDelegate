bd05e11565e886362fe3ab20a2e1ef8ffb8a1b02
Steven Gong <gonghaima@hotmail.com>
2024-01-07 10:58:40 +1100

    first commit

---
+++ CovarianceAndContravarianceDelegate/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CovarianceAndContravarianceDelegate;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# ad-c--3-CovarianceAndContravarianceDelegate
